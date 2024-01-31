f3d77c3d0340a355e675cadefedeb0b8f7b66abb
PrsJulien <83194514+PrsJulien@users.noreply.github.com>
2024-01-31 09:39:17 +0100

    traduction des print

--- Partie3/_main.py
+++ Partie3/_main.py
@@ -13,19 +13,20 @@ gMsg = "The hardest choices require the strongest wills."
 # ---------------------------------------------------------------------------------------
 # -- Substitution algorithm
 # ---------------------------------------------------------------------------------------
+print( "===============================================================================" )
 print( ">>> Substitution algorithm <<<" )
 vKey = rd.choice( [ -1, 1 ] ) * rd.randint( 1, 25 )
 
-print( f"Message to cipher: {gMsg}")
+print( f"Message à chiffrer: {gMsg}")
 print( f"Key: {vKey}")
 
 # -- Cipher algorithm
 vCipheredMsg = s.cipher( gMsg, vKey )
-print( f"Ciphered message: {vCipheredMsg}")
+print( f"Message chiffré: {vCipheredMsg}")
 
 # -- Decipher algorithm
 vDecipheredMsg = s.decipher( vCipheredMsg, vKey )
-print( f"Deciphered message: {vDecipheredMsg}")
+print( f"Message déchiffré : {vDecipheredMsg}")
 # ---------------------------------------------------------------------------------------
 
 
@@ -35,16 +36,16 @@ print( f"Deciphered message: {vDecipheredMsg}")
 print( "\n>>> Affine algorithm <<<" )
 vKey = ( rd.choice( [ 1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25 ] ), rd.randint( 0, 100 ) )
 
-print( f"Message to cipher: {gMsg}")
+print( f"Message à chiffrer: {gMsg}")
 print( f"Key: {vKey}")
 
 # -- Cipher algorithm
 vCipheredMsg = a.cipher( gMsg, vKey )
-print( f"Ciphered message: {vCipheredMsg}")
+print( f"Message chiffré: {vCipheredMsg}")
 
 # -- Decipher algorithm
 vDecipheredMsg = a.decipher( vCipheredMsg, vKey )
-print( f"Deciphered message: {vDecipheredMsg}")
+print( f"Message déchiffré : {vDecipheredMsg}")
 # ---------------------------------------------------------------------------------------
 
 
@@ -55,13 +56,14 @@ print( "\n>>> RSA algorithm <<<" )
 # Générer les clés
 vPrivateKey, vPublicKey = r.generateKeys()
 
-print( f"Message to cipher: {gMsg}")
+print( f"Message à chiffrer : {gMsg}")
 
 # -- Cipher algorithm
 vCipheredMsg = r.cipher( vPublicKey, gMsg )
-print( f"Ciphered message: {vCipheredMsg}")
+print( f"Message chiffré : {vCipheredMsg}")
 
 # -- Decipher algorithm
 vDecipheredMsg = r.decipher( vPrivateKey, vCipheredMsg )
-print( f"Deciphered message: {vDecipheredMsg}")
+print( f"Message déchiffré : {vDecipheredMsg}")
+print( "===============================================================================" )
 # ---------------------------------------------------------------------------------------
